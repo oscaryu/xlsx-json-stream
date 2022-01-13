9719cd4a97136633ae179fdc93f537f2b5a1f9b9
oyu <oscar@sensortransport.com>
2022-01-12 19:54:26 -0800

    reimplement

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const xlsxFile = require('read-excel-file/node');
+const fs = require('fs');
+const XLSX = require('xlsx');
 const { parseExcelDate } = require('read-excel-file');
 
 let MAX_EMPTY_SEQUENTIAL_ROWS = 2;
 let DEFAULT_VALUE='';
-let HEADER_ROW=0;
-let DATA_START_ROW=1;
+let HEADER_ROW=1;
+let DATA_START_ROW=2;
 let INCLUDE_EMPTY=false;
 let SHEETS=[];
 let ASYNC_BATCH_SIZE=1;
 let READ_OPTIONS = { } // dateFormat: 'm/d/yy;@' // mm/dd/yyyy
+let IS_LOCAL_DATES = true;
 
 readToJson = (filename, options, cb) =>{
     return new Promise(async (resolve, reject) => {
@@ -25,19 +28,24 @@ readToJson = (filename, options, cb) =>{
             ASYNC_BATCH_SIZE = options.ASYNC_BATCH_SIZE ?? ASYNC_BATCH_SIZE;
             SHEETS = options.SHEETS ?? SHEETS;
             READ_OPTIONS = options.READ_OPTIONS ?? READ_OPTIONS;
-            DATE_FIELD_LIST = options.DATE_FIELD_LIST ?? [];
             IS_LOCAL_DATES = options.IS_LOCAL_DATES ?? false;
         }
         let emptyRowCtr = 0;
+        /* equivalent to `var wb = XLSX.readFile(filename);` */
+        const buf = fs.readFileSync(filename);
+        const wb = XLSX.read(buf, {type:'buffer'});
+
         if (!SHEETS || !SHEETS.length) {
             let sheets = await xlsxFile(filename, { getSheets: true })
             SHEETS = sheets.map(s=>s.name);
             console.log("No sheets", SHEETS)
         }
+        
         for(let i=0;i<SHEETS.length;i++) {
             const sheetName = SHEETS[i];
+            const worksheet = wb.Sheets[sheetName];
             // console.log("Sheet#"+i, sheetName);
-            await readSheet(filename, sheetName, emptyRowCtr, READ_OPTIONS, options.DATE_FIELD_LIST, IS_LOCAL_DATES, cb);
+            await readSheet(worksheet, sheetName, READ_OPTIONS, IS_LOCAL_DATES, cb);
         }
         resolve();
     });
@@ -45,75 +53,81 @@ readToJson = (filename, options, cb) =>{
 
 module.exports = { readToJson };
 
-function readSheet(filename, sheetName, emptyRowCtr, options, DATE_FIELD_LIST, IS_LOCAL_DATES, cb) {
+function readSheet(worksheet, sheetName, options, IS_LOCAL_DATES, cb) {
     return new Promise(async (resolve, reject) => {
         if (sheetName && sheetName.length) {
             options['sheet'] = sheetName;
         }
         try {
-            await xlsxFile(filename, options).then(async (rows) => {
-                const headers = rows[HEADER_ROW];
-                let promises = [];
-                for (let i = 1; i < rows.length; i++) {
-                    if (i >= DATA_START_ROW) {
-                        const row = rows[i];
-                        let isEmpty = true;
-                        const obj = {};
-                        for (let c = 0; c < row.length; c++) {
-                            const col = row[c];
-                            const hdr = headers[c];
-                            if (col) {
-                                isEmpty = false;
-                                if(typeof col == 'number' && DATE_FIELD_LIST && DATE_FIELD_LIST.length && DATE_FIELD_LIST.includes(hdr)) {
-                                    try {
-                                        if (IS_LOCAL_DATES) {
-                                            obj[hdr] = parseExcelDate(col).toISOString().replace('Z', '');
-                                        } else {
-                                            obj[hdr] = parseExcelDate(col).toISOString();
-                                        }
-                                        // console.log(hdr,":", col,typeof col, 'Is a Date Field:', DATE_FIELD_LIST && DATE_FIELD_LIST.length && DATE_FIELD_LIST.includes(hdr), '; Parsed:', obj[hdr])
-                                    } catch (ex) {
-                                        // console.log(hdr,":", col, 'Unable to parse', ex.toString())
-                                        obj[hdr] = col;
+            const headerList = getHeaderList(worksheet, HEADER_ROW)
+            const headers = Object.keys(headerList).map(x=>x.replace(/[0-9]/gmi,""))
+            console.log('headerList', headers)
+    
+            let emptyRowCtr = 0;
+            let promises = [];
+            for(let i=DATA_START_ROW; i<Number.MAX_SAFE_INTEGER; i++) {
+                const row = {};
+                let isEmptyRow = true;
+                headers.forEach(h=>{
+                    const hdr = headerList[h+''+HEADER_ROW].w;
+                    const cell_value = getCellValue(h+i, worksheet); // 'AD3'
+                    // console.log(cell_value, hdr);
+                    if (cell_value) {
+                        if (typeof cell_value === 'object') {
+                            isEmptyRow = false;
+                            // console.log(i, 'is not empty')
+                            row[hdr] = cell_value.v;
+                            console.log('cell_value', cell_value); // { t: 'n', v: 44373, w: '6/26/21' }
+                            if (cell_value.t === 'n' && (occurrenceCount(cell_value.w, '/') === 2 || occurrenceCount(cell_value.w, '.') === 2 || occurrenceCount(cell_value.w, ':') === 2)) {
+                                try {
+                                    const parsed = parseExcelDate(cell_value.v).toISOString();
+                                    console.log(i, hdr, parsed, cell_value.w);
+                                    if (IS_LOCAL_DATES) {
+                                        row[hdr] = parsed.replace('Z', '');
+                                    } else {
+                                        row[hdr] = parsed;
                                     }
-                                } else {
-                                    obj[hdr] = col;
+                                    // console.log(hdr,":", col,typeof col, 'Is a Date Field:', DATE_FIELD_LIST && DATE_FIELD_LIST.length && DATE_FIELD_LIST.includes(hdr), '; Parsed:', obj[hdr])
+                                } catch (ex) {
+                                    // console.log(hdr,":", col, 'Unable to parse', ex.toString())
+                                    row[hdr] = cell_value;
                                 }
-                            } else if (DEFAULT_VALUE != null) {
-                                obj[hdr] = DEFAULT_VALUE;
-                            }
-                        }
-                        if (isEmpty) {
-                            emptyRowCtr += 1;
-                            // console.log(i, isEmpty, row.length, headers.length, "Empty row detected");
-                            if (emptyRowCtr >= MAX_EMPTY_SEQUENTIAL_ROWS) {
-                                // console.log("Done");
-                                break;
-                            }
-                            if (INCLUDE_EMPTY) {
-                                promises.push(cb(null, {row:obj, rowCtr:i, sheetName, dataLength:headers.length, headerLength:row.length}));
-                                if (promises.length>= ASYNC_BATCH_SIZE) {
-                                    await Promise.all(promises);
-                                    promises = [];
-                                }
+                            } else {
+                                row[hdr] = cell_value.v;
                             }
                         } else {
-                            emptyRowCtr = 0;
-                            // console.log(i, isEmpty, row.length, headers.length, obj);
-                            promises.push(cb(null, {row:obj, rowCtr:i, sheetName, dataLength:headers.length, headerLength:row.length}));
-                            if (promises.length>= ASYNC_BATCH_SIZE) {
-                                await Promise.all(promises);
-                                promises = [];
-                            }
+                            row[hdr] = cell_value;
+                        }
+                    } else if (INCLUDE_EMPTY) {
+                        row[hdr] = DEFAULT_VALUE;
                     }
+                })
+                if (isEmptyRow) {
+                    emptyRowCtr = emptyRowCtr+1;
+                    // console.log(i, 'is empty.  emptyCount:', emptyCount)
+                } else {
+                    // console.log(i, 'is not empty')
+                    emptyRowCtr = 0;
+                    //row['row'] = i;
+                }
+                if (row !== {} && Object.keys(row).length) {
+                    // console.log("Parsed Obj",i+":", row);
+                    // await cb(null, obj);
+                    promises.push(cb(null, {row, rowCtr:i, sheetName, dataLength:headers.length, headerLength:row.length}));
+                    if (promises.length>= ASYNC_BATCH_SIZE) {
+                        await Promise.all(promises);
+                        promises = [];
                     }
                 }
-                if (promises.length>= 0) {
-                    await Promise.all(promises);
-                    promises = [];
+                if (emptyRowCtr>= MAX_EMPTY_SEQUENTIAL_ROWS) {
+                    // console.log("BREAKING!!!", i, emptyRowCtr, MAX_EMPTY_SEQUENTIAL_ROWS)
+                    resolve();
+                    break;
                 }
-                resolve();
-            });
+            }
+            if (promises.length>= 0) {
+                await Promise.all(promises);
+            }
         } catch (ex) {
             const str = ex.toString();
             if (str.indexOf('Error: Sheet') > -1 && str.indexOf('not found in the') > -1) {
@@ -122,7 +136,42 @@ function readSheet(filename, sheetName, emptyRowCtr, options, DATE_FIELD_LIST, I
                 console.log('********', ex.toString());
             }
         }
+
         resolve();
     });
 }
 
+function getHeaderList(worksheet, lineNbr) {
+    let keys = Object.keys(worksheet);
+    let headerList = {};
+    let headers = keys.filter(x=>x.replace(/[A-Z]/gmi,"")==lineNbr);
+    headers.forEach(x=>{
+        const desired_cell = worksheet[x];
+        headerList[x] = desired_cell
+    })
+    return headerList;
+}
+
+function getCellValue(address_of_cell, worksheet) {
+    const cell = worksheet[address_of_cell];
+    let desired_value = undefined;
+    if (cell) {
+        if (cell.w && cell.v && cell.w != cell.v) {
+            desired_value = cell;
+        } else {
+            desired_value = cell.w;
+        }
+    }
+    return desired_value;
+}
+
+function occurrenceCount(str, char)
+{
+    let ctr = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == char) {
+            ctr = ctr + 1;
+        }
+    }
+    return ctr;
+}
